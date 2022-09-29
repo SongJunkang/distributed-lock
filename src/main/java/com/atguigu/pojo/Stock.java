7f766b30d0c75ac5e5deaedbe1ce44e6e53b745c
Song <872064814@qq.com>
2022-09-29 11:14:54 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/pojo/Stock.java
@@ -0,0 +1,15 @@
+package com.atguigu.pojo;
+
+import lombok.Data;
+
+/**
+ * @author joakims
+ * @create 2022-09-24-21:28
+ **/
+
+@Data
+public class Stock {
+
+    private Integer stock = 5000;
+
+}
---
+++ src/main/java/com/atguigu/service/StockService.java
@@ -0,0 +1,19 @@
+package com.atguigu.service;
+
+import com.atguigu.pojo.Stock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author joakims
+ * @create 2022-09-24-21:45
+ **/
+
+@Service
+public class StockService {
+
+
+    private Stock stock  = new Stock();
+
+
+}
